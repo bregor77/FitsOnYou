224757b71474adf6cafc98e3078a816bf3329064
bregor77 <bregor77@gmail.com>
2023-08-07 20:50:00 +0100

    commit 1

---
+++ UnitTestEx0/DemoTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UnitTestEx0
+{
+    public class DemoTest
+    {
+        public string fname;
+        public string lname;
+        public DemoTest() 
+        {
+            this.fname = "Mariusz";
+            this.lname = "Kucharski";
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using UnitTestEx0;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DemoTest obj = new DemoTest();
+            Assert.AreEqual("Mariusz", obj.fname);
+            Assert.AreEqual("Kucharski", obj.lname);
+        }
+    }
+}
